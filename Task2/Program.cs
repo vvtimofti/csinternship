36564e6fcd1688f23c62194775f6004d29665490
Timofti Vladimir <vova.timofti7@gmail.com>
2024-01-18 20:21:19 +0200

    second task

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        static string GetSubsequence(string sequence)
+        {
+            int end = sequence.Length - 1;
+            int start = end - 1;
+            List<string> subseq = [];
+            while (end > 0)
+            {
+                if (start > -1 && sequence[start + 1] != sequence[start])
+                {
+                    subseq.Add(sequence.Substring(start, end - start + 1));
+                    start--;
+                }
+                else
+                {
+                    end--;
+                    start = end - 1;
+                }
+            }
+            return String.Join(", ", subseq.ToArray());
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a sequence: ");
+            string userInput = Console.ReadLine();
+            string res = GetSubsequence(userInput);
+            Console.WriteLine(res);
         }
     }
 }
