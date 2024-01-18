7e968eb38142e34e62e82e2276b7237cd6c5691a
Timofti Vladimir <vova.timofti7@gmail.com>
2024-01-18 20:18:16 +0200

    first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Task1
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
 {
     internal class Program
     {
+        static string GetCellColor(string cell)
+        {
+            string pattern = "^[A-Ha-h][1-8]$";
+
+            if (!Regex.IsMatch(cell, pattern))
+            {
+                return "Invalid pattern.";
+            }
+
+            return (cell[0] + cell[1]) % 2 == 1 ? "White" : "Black";
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput, result;
+            while (true)
+            {
+                Console.Write("\nEnter the cell ('q' to exit): ");
+                userInput = Console.ReadLine();
+                if (userInput.Equals("q")) break;
+                result = GetCellColor(cell: userInput);
+                Console.WriteLine(result);
+            }
         }
     }
 }
